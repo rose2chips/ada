984c26e8221e37ce631a0dda76318503e687fbc1
Rose Chips <rose2chips@gmail.com>
2021-01-18 15:49:14 +0900

    Refactoring

--- scripts/11.p1-calculate-startKesPeriod.sh
+++ scripts/11.p1-calculate-startKesPeriod.sh
@@ -11,21 +11,26 @@ kesPeriod=$((${slotNo} / ${slotsPerKESPeriod}))
 #echo kesPeriod: ${kesPeriod}
 startKesPeriod=${kesPeriod}
 echo startKesPeriod: ${startKesPeriod}
+#echo ${startKesPeriod} > $NODE_HOME/startKesPeriod
 
 cat > $HOME/ada/setup/12.s2-gen-op-cert.sh << EOF
-fromproducer.sh ada/cardano-node/kes.vkey $HOME/cold-keys/
-fromproducer.sh ada/cardano-node/startKesPeriod $HOME/ada/setup/
+#pushd $HOME/cold-keys
+cd \$HOME/cold-keys
 
-pushd $HOME/cold-keys
+rm -f kes.vkey
+rm -f kes.skey
+
+fromproducer.sh ada/cardano-node/kes.vkey
+fromproducer.sh ada/cardano-node/kes.skey
 
 cardano-cli node issue-op-cert \\
     --kes-verification-key-file kes.vkey \\
-    --cold-signing-key-file $HOME/cold-keys/node.skey \\
-    --operational-certificate-issue-counter $HOME/cold-keys/node.counter \\
+    --cold-signing-key-file node.skey \\
+    --operational-certificate-issue-counter node.counter \\
     --kes-period ${startKesPeriod} \\
     --out-file node.cert
 
-toproducer.sh $HOME/cold-keys/node.cert ada/cardano-node/
+toproducer.sh node.cert ada/cardano-node/
 
 echo "Be sure to BACK UP ALL your KEYS to another secure storage device."
 echo "Make MULTIPLE COPIES."
