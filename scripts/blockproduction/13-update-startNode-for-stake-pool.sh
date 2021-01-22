b44ba6744c7212c944b33f994f5ecd73321ec8cc
Rose Chips <rose2chips@gmail.com>
2021-01-22 11:50:57 +0000

    Fix HOSTADDR

--- scripts/blockproduction/13-update-startNode-for-stake-pool.sh
+++ scripts/blockproduction/13-update-startNode-for-stake-pool.sh
@@ -5,7 +5,7 @@ sudo systemctl stop cardano-node
 cat > $NODE_HOME/startNode.sh << EOF
 DIRECTORY=$NODE_HOME
 PORT=6000
-HOSTADDR=<IP ADDR>
+HOSTADDR=0.0.0.0
 TOPOLOGY=\${DIRECTORY}/${NODE_CONFIG}-topology.json
 DB_PATH=\${DIRECTORY}/db
 SOCKET_PATH=\${DIRECTORY}/db/socket
