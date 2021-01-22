6aa1b63cba499035791b788b5e32fdd8d9f6b11b
Rose Chips <rose2chips@gmail.com>
2021-01-22 12:33:17 +0000

    Remove obsolete code

--- scripts/base/5.update-env-variables.sh
+++ scripts/base/5.update-env-variables.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
   
-#echo PATH="$HOME/.local/bin:$PATH" >> $HOME/.bashrc
-#export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-#export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
-echo export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" >> $HOME/.bashrc
-echo export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH" >> $HOME/.bashrc
-#echo export NODE_HOME=$HOME/cardano-my-node >> $HOME/.bashrc
+#echo export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" >> $HOME/.bashrc
+#echo export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH" >> $HOME/.bashrc
 echo export NODE_HOME=$HOME/ada/cardano-node >> $HOME/.bashrc
 echo export NODE_CONFIG=mainnet>> $HOME/.bashrc
 echo export NODE_BUILD_NUM=$(curl https://hydra.iohk.io/job/Cardano/iohk-nix/cardano-deployment/latest-finished/download/1/index.html | grep -e "build" | sed 's/.*build\/\([0-9]*\)\/download.*/\1/g') >> $HOME/.bashrc
