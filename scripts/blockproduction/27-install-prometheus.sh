1cabd1d741afedd73130c42f21e63b55e221f2ff
Rose Chips <rose2chips@gmail.com>
2021-01-22 15:05:13 +0000

    Add 27-install-prometheus

---
+++ scripts/blockproduction/27-install-prometheus.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y prometheus-node-exporter 
+
+sudo systemctl enable prometheus-node-exporter.service
+
+#sudo ufw allow from <IP ADDR of relay node> to any port 12798
+#sudo ufw allow from <IP ADDR of relay node>  to any port 9100
+#sudo ufw allow from <IP ADDR of relay node>  to any port 90910
---
+++ scripts/relay/27-install-prometheus-and-grafana.sh
@@ -0,0 +1,18 @@
+sudo apt-get install -y prometheus prometheus-node-exporter
+
+cd $NODE_HOME/
+mkdir grafana
+cd grafana
+
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+echo "deb https://packages.grafana.com/oss/deb stable main" > grafana.list
+sudo mv grafana.list /etc/apt/sources.list.d/grafana.list
+
+sudo apt-get update && sudo apt-get install -y grafana
+
+sudo systemctl enable grafana-server.service
+sudo systemctl enable prometheus.service
+sudo systemctl enable prometheus-node-exporter.service
+
+#sudo ufw allow from <YOUR IP ADDR> to any port 3000
