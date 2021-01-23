67bd9223ac1468b4ec0bc66010e7ebf165634bbd
Rose Chips <rose2chips@gmail.com>
2021-01-23 03:41:57 +0000

    Update update-node-configuration

---
+++ scripts/cmds/update-node-configuration.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd $NODE_HOME
+
+if ! [ -d "backup" ]; then
+  mkdir backup
+fi 
+
+mv ${NODE_CONFIG}-byron-genesis.json backup/
+mv ${NODE_CONFIG}-shelley-genesis.json backup/
+mv ${NODE_CONFIG}-config.json backup/
+
+NODE_BUILD_NUM=$(curl https://hydra.iohk.io/job/Cardano/iohk-nix/cardano-deployment/latest-finished/download/1/index.html | grep -e "build" | sed 's/.*build\/\([0-9]*\)\/download.*/\1/g')
+echo NODE_BUILD_NUM=${NODE_BUILD_NUM} at $(date) >> logs/node_build_num.log
+
+wget -N https://hydra.iohk.io/build/${NODE_BUILD_NUM}/download/1/${NODE_CONFIG}-byron-genesis.json
+#wget -N https://hydra.iohk.io/build/${NODE_BUILD_NUM}/download/1/${NODE_CONFIG}-topology.json
+wget -N https://hydra.iohk.io/build/${NODE_BUILD_NUM}/download/1/${NODE_CONFIG}-shelley-genesis.json
+wget -N https://hydra.iohk.io/build/${NODE_BUILD_NUM}/download/1/${NODE_CONFIG}-config.json
+
+sed -i ${NODE_CONFIG}-config.json \
+    -e "s/TraceBlockFetchDecisions\": false/TraceBlockFetchDecisions\": true/g"
+
+# for Prometheus
+sed -i ${NODE_CONFIG}-config.json -e "s/127.0.0.1/0.0.0.0/g"
