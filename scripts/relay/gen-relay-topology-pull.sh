ba80d80f4eb7f7a69ca945d43f32a98dfd49bd14
Rose Chips <rose2chips@gmail.com>
2021-01-26 00:52:53 +0000

    Fix 5.update-env-variables.sh

--- scripts/relay/gen-relay-topology-pull.sh
+++ scripts/relay/gen-relay-topology-pull.sh
@@ -20,7 +20,7 @@ EOF
 
 #crontab -l | cat - crontab-fragment.txt >crontab.txt && crontab crontab.txt
 #rm crontab-fragment.txt
-crontab -l | cat - crontab-fragment.txt > crontab.txt
+crontab -l | cat - crontab-fragment.txt >> crontab.txt
 
 cat > $NODE_HOME/restart-node.sh << EOF
 #!/bin/bash
@@ -33,4 +33,4 @@ chmod +x $NODE_HOME/restart-node.sh
 cat > crontab-sudo-fragment.txt << EOF
 00 00 * * * ${NODE_HOME}/restart-node.sh
 EOF
-sudo crontab -l | cat - crontab-sudo-fragment.txt > crontab-sudo.txt
+sudo crontab -l | cat - crontab-sudo-fragment.txt >> crontab-sudo.txt
